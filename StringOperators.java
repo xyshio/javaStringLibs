b0d183c857256c3819eede7ab8c7b95936bc1c49
xyshio <xyshio@gmail.com>
2017-03-15 13:41:04 +0100

    Create StringOperators.java

---
+++ StringOperators.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringOperators {
+
+
+  	  public static String getTextBetweenStringsUsingRegexp(String input, String startMatch, String endMatch){
+          Matcher matcher = Pattern.compile(startMatch+"(.*)"+endMatch).matcher(input);
+          if (matcher.find()) {
+              return matcher.group(1);
+          }
+          return "";
+      }
+
+    
+    public static String getSandwichUsingRegexp(String input){
+          Matcher matcher = Pattern.compile("chleb(.*)chleb").matcher(input);
+          if (matcher.find()) {
+              return matcher.group(1);
+          }
+          return "";
+      }
+    
+    
+        public static String getTextBetweenUsingIndexOf(String input, String starter, String ender) {
+          int firstIndex = input.indexOf(starter);
+          int lastIndex = input.lastIndexOf(ender);
+          if (firstIndex < lastIndex) {
+              return input.substring(firstIndex + starter.length(), lastIndex);
+          }
+          return "";
+      }
+      public static  String getTextBetweenSameStartEndUsingSplit(String input, String sameStartEndText){
+         String[] ingredients =input.split(sameStartEndText,-2);
+          if(ingredients.length>=3) {
+              String[] ingredientsInside = Arrays.copyOfRange(ingredients, 1, ingredients.length - 1);
+              return Arrays.stream(ingredientsInside).collect(Collectors.joining(sameStartEndText));
+          }
+          return "";
+      }
+}
